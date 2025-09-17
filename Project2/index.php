89a57ca091f303a158fe6842ec5959a30bec70db
Ana Reis <157768368+Way9e@users.noreply.github.com>
2025-09-17 21:06:56 +0100

    index.php

---
+++ Project2/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Website criado para a participaçao no worldskill regional">
+    <meta name="keywords" content="worldskills, skills, Ana Reis, hmtl, css, php">
+    <meta name="author" content="Ana Reis">
+    <title>Gestão de Telecomunicações Móveis</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/x-icon" href="img/WSPT_70anos_af3.png">
+</head>
+<body>
+    <!--Secção1 Menu de Navegação-->
+    <nav>
+        <img src="img/WSPT_70anos_af3.png" alt="WorldSkills Portugal 70 Anos">
+        <div class="links">
+          <a href="listarUser.php">Utilizadores</a>
+          <a href="listarLocalidades.php">Localidades</a>
+          <a href="listarCliente.php">Clientes</a>
+          <a href="listarChamada.php">Chamadas</a>
+          <a href="listarMarca.php">Marcas</a>
+          <a href="listarTelemovel.php">Telemoveis</a>
+        </div>
+    </nav>
+    
+    <footer>
+      <small>Copyright © 2024 – WorldSkills Portugal</small>
+      <div id="redes">
+        <a href="http://www.facebook.com/#"><i class="bi bi-facebook"></i>FaceBook</a>
+        <a href="http://www.twitter.com/#"><i class="bi bi-twitter"></i>Twitter</a>
+        <a href="http://www.youtube.com/#"><i class="bi bi-youtube"></i>Youtube</a>
+      </div>
+    </footer>
+</body>
+</html>
